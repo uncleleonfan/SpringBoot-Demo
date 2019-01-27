998d8ed58b897620a2b70bf9103772aadee48c9d
Leon <526247082@qq.com>
2019-01-27 20:24:59 +0800

    Setup

---
+++ src/main/java/com/springboot/demo/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Spring Boot入门文件
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/springboot/demo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.springboot.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        Logger.getAnonymousLogger().log(Level.INFO, "index");
+        return "index";
+    }
+}
