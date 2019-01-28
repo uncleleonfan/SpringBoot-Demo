7d2f15074d1b4e8f94a020ffd68cdb8282b1bc86
byex_888 <604217454@qq.com>
2019-01-28 15:14:05 +0800

    remove useless things.

--- src/main/java/com/springboot/demo/controller/IndexController.java
+++ src/main/java/com/springboot/demo/controller/IndexController.java
@@ -12,7 +12,6 @@ public class IndexController {
 
     @RequestMapping("/index")
     public String index() {
-        Logger.getAnonymousLogger().log(Level.INFO, "index");
         return "index";
     }
 }
